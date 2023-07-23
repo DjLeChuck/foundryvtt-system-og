ba1e8f98da08821c4f4dbdc2de94b51b7e775d8e
Vivien DE BONA <vdebona@umanit.fr>
2023-07-23 20:05:34 +0200

    feat: simple redesign of creature sheet

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -12,6 +12,7 @@ export const preloadHandlebarsTemplates = async function () {
     'systems/og/templates/actor/parts/actor-tab-words.html.hbs',
     'systems/og/templates/actor/parts/actor-words.html.hbs',
     'systems/og/templates/actor/parts/actor-attacks.html.hbs',
+    'systems/og/templates/actor/parts/actor-unggghh-points.html.hbs',
     // Item partials.
     'systems/og/templates/item/parts/item-effects.html.hbs',
   ]);
